89d917c8545de4006cee6e4bd8d5d54405e1f22c
okutasan <oktavanhowen@gmail.com>
2018-04-07 22:15:52 +0700

    add component

---
+++ src/button.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+    render(){
+        return(
+            <button>Klik Saya</button>
+        );
+    }
+}
+
+export default Button;
