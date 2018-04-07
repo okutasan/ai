9f929c11ee16bdddbed1234d60bff54493fedefe
okutasan <oktavanhowen@gmail.com>
2018-04-07 22:40:47 +0700

    State

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
-
 import './App.css';
 import Button from './button';
 
 class App extends Component {
+constructor(){
+  super();
+  this.state={isi:[]}
+}
+
+componentDidMount(){
+  this.setState({isi:['Klik','me','senpai!']})
+}
   render() {
+    let i=1;
+    let button = this.state.isi.map(value => (
+      <Button key={i++} isi = {value}/>
+    ));
+
     return (
       <div>
         <h1> Hello World </h1>
         <p> Welcome to my web </p>
-        <Button/>
+        {button}
       </div>
       
     );
--- src/button.jsx
+++ src/button.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Button extends Component {
     render(){
         return(
-            <button>Klik Saya</button>
+            <button>{this.props.isi}</button>
         );
     }
 }
