8f4ff9e96f91ff6d360c8d65f6ef6a1aa0eb7b05
okutasan <oktavanhowen@gmail.com>
2018-04-11 02:02:03 +0700

    About + Contact done

---
+++ src/About.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <center>
+                    <p>Shizukasa ya, hanasaki niwa no, haru no ame.</p>
+                </center>
+            </div>
+        );
+    }
+}
+export default About;
